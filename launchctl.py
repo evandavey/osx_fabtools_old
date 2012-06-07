a1468a4f442b6e8837b56988a11aeeacf08ce7ca
Evan Davey <evan.davey@cochranedavey.com>
2012-06-07 14:21:00 +0100

    adds templating to launchctl

--- launchctl.py
+++ launchctl.py
@@ -4,6 +4,25 @@ Launchctl fabric commands
 
 from fabric.api import *
 from fabric.colors import *
+from fabric.contrib.files import exists,upload_template
+import os
+
+def install2(label,args,dst='/Library/LaunchDaemons'):
+    """
+    Creates and installs a launchdaemon with the specified args array
+    """
+    
+    runAtLoad=True
+    keepAlive=True
+    
+    dst_file=os.path.join(dst,label+'.plist')
+    with settings(warn_only=True):
+        unload(label)
+        upload_template('launchdaemon.plist', dst_file, backup = False, context=locals(), use_jinja=True, use_sudo=True, template_dir='templates')
+        sudo('chown root:admin %s' % (dst_file))
+        
+        load(label)
+        cmd('start',label)
 
 def install(label,src,dst):
     
@@ -20,8 +39,11 @@ def install(label,src,dst):
 def load(label):
     sudo('launchctl load %s/%s.plist' % ('/Library/LaunchDaemons/',label))
     
-def unload(label):
-    sudo('launchctl unload %s/%s.plist' % ('/Library/LaunchDaemons/',label))
+def unload(label, dst='/Library/LaunchDaemons'):
+    
+    dst_file=os.path.join(dst,label+'.plist')
+    if exists(dst_file):
+        sudo('launchctl unload %(dst_file)s' % locals())
 
 def delete(label):
     unload(label)
